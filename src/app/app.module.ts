b7761d3bfec6bff30357a15675700afb1226b42c
danielpaluch <danielpaluch37@gmail.com>
2024-08-05 18:01:13 +0200

    merges of missed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ShellRoutingModule } from './shell/shell-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -9,11 +12,20 @@ import { ShellModule } from './shell/shell.module';
 import { HttpInterceptorProviders } from './http-interceptors';
 import { LoginRoutingModule } from './login/login.routing-module';
 
-@NgModule({ declarations: [AppComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        ShellRoutingModule,
-        StoreModule.forRoot({}, {}),
-        BrowserAnimationsModule,
-        LoginRoutingModule
-        ShellModule], providers: [HttpInterceptorProviders, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    ShellRoutingModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule,
+    LoginRoutingModule,
+    ShellModule,
+  ],
+  providers: [
+    HttpInterceptorProviders,
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+})
 export class AppModule {}
