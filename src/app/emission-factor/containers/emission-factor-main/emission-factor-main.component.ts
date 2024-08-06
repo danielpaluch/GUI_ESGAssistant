671e5a3878a8ed786e7a00274079378b301d7510
Mateusz Zato <mateusz.zaton@easysolar-app.com>
2024-08-06 21:24:40 +0200

    fix: unsubscribe to dialog ref

--- src/app/emission-factor/containers/emission-factor-main/emission-factor-main.component.ts
+++ src/app/emission-factor/containers/emission-factor-main/emission-factor-main.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmissionFactor } from '../../models/emission-form.model';
 import { DialogAddEmissionFactorComponent } from '../../components/dialog-add-emission-factor/dialog-add-emission-factor.component';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-emission-factor-main',
@@ -11,13 +12,14 @@ import { DialogAddEmissionFactorComponent } from '../../components/dialog-add-em
 export class EmissionFactorMainComponent {
   components: EmissionFactor[] = [];
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private readonly dialog: MatDialog, private readonly destroyRef: DestroyRef) {}
 
   public addEmissionFactor(): void {
     const dialogRef = this.dialog.open(DialogAddEmissionFactorComponent);
 
     dialogRef
       .afterClosed()
+      .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe((data: EmissionFactor | undefined | null) => {
         if (data) {
           this.components.push(data);
